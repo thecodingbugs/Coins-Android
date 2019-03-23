ed837bd744aa1736074e7a2edc2b0799c9948773
Jeremy <jeremy.ninan25@hotmail.com>
2019-03-23 22:40:45 +0530

    activities added

--- app/src/main/java/com/android/coins/MainActivity.java
+++ app/src/main/java/com/android/coins/MainActivity.java
@@ -2,13 +2,25 @@ package com.android.coins;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MSG = "Display this";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SendMessage(View view){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.txtMessage);
+        String msg = editText.getText().toString();
+        intent.putExtra(EXTRA_MSG, msg);
+        startActivity(intent);
     }
 }
